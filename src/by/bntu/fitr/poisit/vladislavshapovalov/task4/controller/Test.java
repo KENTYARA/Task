6efe4c2a7a5c4381f9b8b32bfd805a6603739e73
VLAD <47355654+KENTYARA@users.noreply.github.com>
2019-04-23 11:46:28 +0300

    Update Test.java

--- src/by/bntu/fitr/poisit/vladislavshapovalov/task4/controller/Test.java
+++ src/by/bntu/fitr/poisit/vladislavshapovalov/task4/controller/Test.java
@@ -1,17 +1,9 @@
 package by.bntu.fitr.poisit.vladislavshapovalov.task4.controller;
 
 import by.bntu.fitr.poisit.vladislavshapovalov.task4.model.entity.Matrix;
-import static by.bntu.fitr.poisit.vladislavshapovalov.task4.model.logic.MatrixLogic.inputMatrix;
-import static by.bntu.fitr.poisit.vladislavshapovalov.task4.model.logic.MatrixLogic.read;
-import static by.bntu.fitr.poisit.vladislavshapovalov.task4.model.logic.MatrixLogic.matrixToString;
-import static by.bntu.fitr.poisit.vladislavshapovalov.task4.model.logic.MatrixLogic.checkSymmetry;
-import static by.bntu.fitr.poisit.vladislavshapovalov.task4.model.logic.MatrixLogic.getMaxToMatrix;
-import static by.bntu.fitr.poisit.vladislavshapovalov.task4.model.logic.MatrixLogic.getMinToMatrix;
-import org.apache.log4j.Appender;
-import org.apache.log4j.Layout;
-import org.apache.log4j.ConsoleAppender;
+import by.bntu.fitr.poisit.vladislavshapovalov.task4.model.logic.MatrixLogic;
 import org.apache.log4j.Logger;
-import org.apache.log4j.SimpleLayout;
+
 /**
  *
  * @author MICKEY
@@ -30,21 +22,21 @@ public class Test {
     public static void main(String[] args) {
         
     
-        int height = read("Enter the number of rows of your matrix: ", 1, 20);
-        int width = read("Enter the number of columns of your matrix: ", 1, 20);
-
-        int[][] matrix = inputMatrix(height, width, -10, 10);
+        int height = MatrixLogic.read("Enter the number of rows of your matrix: ", 1, 20);
+        int width = MatrixLogic.read("Enter the number of columns of your matrix: ", 1, 20);
+        Matrix matrix = new Matrix(height,width);
+        
+        MatrixLogic.createRandomized(matrix);
  
-        LOG.trace(matrixToString(matrix, 2));
-        LOG.trace("The minimum value of the matrix:  " + getMinToMatrix(matrix));
-        LOG.trace("The maximum value of the matrix: " + getMaxToMatrix(matrix));
-        LOG.trace("Is the matrix symmetric with respect to the main diagonal?: " + checkSymmetry(matrix));
+        LOG.trace(matrix);
+        
+        LOG.trace("Is the matrix symmetric with respect to the main diagonal?: " + MatrixLogic.checkSymmetry(matrix));
  
         LOG.trace("Replace all local minima with 0: ");
-        replaceLocalMinimum(matrix, 0);
-        LOG.trace(matrixToString(matrix, 2));
+        MatrixLogic.replaceLocalMinimum(matrix, 0);
+        LOG.trace(matrix);
  
-        LOG.trace("Is the matrix symmetric about the main diagonal now?: " + checkSymmetry(matrix));
+        LOG.trace("Is the matrix symmetric about the main diagonal now?: " + MatrixLogic.checkSymmetry(matrix));
  
     }
 }
