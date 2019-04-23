e5dfc4fc5129f535168d66688c5515e2e4b672d4
VLAD <47355654+KENTYARA@users.noreply.github.com>
2019-04-23 11:45:48 +0300

    Update MatrixLogic.java

--- src/by/bntu/fitr/poisit/vladislavshapovalov/task4/model/logic/MatrixLogic.java
+++ src/by/bntu/fitr/poisit/vladislavshapovalov/task4/model/logic/MatrixLogic.java
@@ -17,16 +17,15 @@ public class MatrixLogic {
     }
     
     
-    public static int[][] inputMatrix(int height, int width, int minRange, int maxRange) {
-        int[][] matrix = new int[height][width];
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                String text = String.format("Enter matrix element [%d][%d]: ", i, j);
-                matrix[i][j] = read(text, minRange, maxRange);
+    public static Matrix createRandomized(Matrix matrix){
+        for(int i = 0; i < matrix.getCountRows(); i++){
+            for(int j = 0; j < matrix.getCountColumns(); j++){
+                matrix.setElement(i, j, (int)(Math.random() * matrix.getCountColumns()) + 1);
             }
         }
         return matrix;
     }
+   
  
     public static boolean checkSymmetry(Matrix matrix) { 
         boolean result;
@@ -43,31 +42,19 @@ public class MatrixLogic {
         return result;
     }
  
-    public static int getMaxToMatrix(Matrix matrix) { 
-        int max = 0;
-        if (matrix != null && matrix.getCountRows() > 0 && matrix.getCountColumns() > 0) {
-            max = matrix.getElement(0, 0);
-            for (int[] line : matrix) {
-                for (int number : line) {
-                    if (max < number) {
-                        max = number;
-                    }
-                }
-            }
-        }
-        return max;
-    }
  
     public static void replaceLocalMinimum(Matrix matrix, int replaceNumber) {
         for (int i = 0; i < matrix.getCountRows(); i++) {
             for (int j = 0; j < matrix.getCountColumns(); j++) {
                 if (isLocalMinimum(matrix, i, j)) {
-                    matrix.getElement(i, j) = replaceNumber;
+                    matrix.setElement(i, j,replaceNumber);
                 }
             }
         }
     }
  
+    
+    
     public static boolean isLocalMinimum(Matrix matrix, int i, int j) { 
         boolean result = true;
         for (int k = i - 1; k <= i + 1; k++) {
@@ -87,33 +74,6 @@ public class MatrixLogic {
         return result;
     }
  
-    public static int getMinToMatrix(Matrix matrix) { 
-        int min = 0;
-        if (matrix != null && matrix.getCountRows() > 0 && matrix.getCountColumns() > 0) {
-            min = matrix.getElement(0, 0);
-            for (int[] line : matrix) {
-                for (int number : line) {
-                    if (min > number) {
-                        min = number;
-                    }
-                }
-            }
-        }
-        return min;
-    }
- 
-    public static String matrixToString(Matrix matrix, int maxLengthNumber) {
-        StringBuilder sb = new StringBuilder();
-        String tab = String.format("%s%d%s", "%", maxLengthNumber + 2, "d");
-        for (int[] line : matrix) {
-            sb.append("|");
-            for (int number : line) {
-                sb.append(String.format(tab, number));
-            }
-            sb.append("|").append("\n");
-        }
-        return sb.toString();
-    }
 
     public static int read(String text, int min, int max) {
         int result = 0;
